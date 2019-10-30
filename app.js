c4bea69b367c6ee5ba9c6815e1e15a8cd13b51ba
Angelika Tyburska <angelikatyburska@gmail.com>
2019-10-30 20:52:19 +0100

    Modified .js

--- app.js
+++ app.js
@@ -22,52 +22,134 @@ document.addEventListener("DOMContentLoaded", function () {
     const val4 = fourthRow.querySelector(".row-value");
     const val5 = fifthRow.querySelector(".row-value");
 
-    quantity.addEventListener("click", function () {
-        const value1 = 0.5;
-        const quantityValue = this.value;
-        val1.innerText = "$ " + quantityValue * value1;
-        qty1.innerText = `${quantityValue} *$${value1}`;
-        firstRow.style.visibility = "visible";
-    });
-    order.addEventListener("click", function (e) {
-        const value2 = 0.25;
-        const orderValue = this.value;
-        val2.innerText = "$ " + orderValue * value2;
-        qty2.innerText = `${orderValue} *$${value2}`;
-        secondRow.style.visibility = "visible";
-    });
-    package.addEventListener("click", function () {
-        const packageValue = this.value;
-        qty3.innerText = packageValue;
-        thirdRow.style.visibility = "visible";
-        const prices = [0, 25, 60];
-        if (packageValue === "Basic") {
-            val3.innerText = "$" + prices[0];
-        } else if (packageValue === "Professional") {
-            val3.innerText = "$" + prices[1];
-        } else if (packageValue === "Premium") {
-            val3.innerText = "$" + prices[2];
-        }
-    })
-    accounting.addEventListener("click", function () {
-        const value4 = 35;
-        val4.innerText = "$" + value4;
-        if (accounting.checked) {
-            fourthRow.style.visibility = "visible";
-        }
-    })
-    rental.addEventListener("click", function () {
-        const value5 = 5;
-        val5.innerText = "$" + value5;
-        if (rental.checked) {
-            fifthRow.style.visibility = "visible";
-        }
-    });
-    const all = document.querySelectorAll(".row-value");
-    console.log(all);
-    for (let i = 0; i < all.length; i++) {
-        let singleAll = all[i];
+    const inputs = document.querySelector(".orders").querySelectorAll(".calc-input, .calc-checkbox");
+    console.log(inputs);
+    for (let i = 0; i < inputs.length; i++) {
+        // let singleElement = inputs[i];
+        inputs[i].addEventListener("click", function () {
+            const button = document.querySelector(".value-btn").lastElementChild;
+            const quantity = inputs[0];
+            if (i === 0) {
+                const firstRow = document.getElementById("first-row");
+                const qty1 = firstRow.querySelector(".row-calculation");
+                const val1 = firstRow.querySelector(".row-value");
 
+
+
+            };
+
+
+
+
+        });
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //     quantity.addEventListener("click", function () {
+    //         const value1 = 0.5;
+    //         const quantityValue = this.value;
+    //         val1.innerText = "$ " + quantityValue * value1;
+    //         qty1.innerText = `${quantityValue} *$${value1}`;
+    //         firstRow.style.visibility = "visible";
+    //     });
+    //     order.addEventListener("click", function (e) {
+    //         const value2 = 0.25;
+    //         const orderValue = this.value;
+    //         val2.innerText = "$ " + orderValue * value2;
+    //         qty2.innerText = `${orderValue} *$${value2}`;
+    //         secondRow.style.visibility = "visible";
+    //     });
+    //     package.addEventListener("click", function () {
+    //         const packageValue = this.value;
+    //         qty3.innerText = packageValue;
+    //         thirdRow.style.visibility = "visible";
+    //         const prices = [0, 25, 60];
+    //         if (packageValue === "Basic") {
+    //             val3.innerText = "$" + prices[0];
+    //         } else if (packageValue === "Professional") {
+    //             val3.innerText = "$" + prices[1];
+    //         } else if (packageValue === "Premium") {
+    //             val3.innerText = "$" + prices[2];
+    //         }
+    //     })
+    //     accounting.addEventListener("click", function () {
+    //         const value4 = 35;
+    //         val4.innerText = "$" + value4;
+    //         if (accounting.checked) {
+    //             fourthRow.style.visibility = "visible";
+    //         }
+    //     })
+    //     rental.addEventListener("click", function () {
+    //         const value5 = 5;
+    //         val5.innerText = "$" + value5;
+    //         if (rental.checked) {
+    //             fifthRow.style.visibility = "visible";
+    //         }
+    //     });
+    //     const all = document.querySelectorAll(".row-value");
+    //     console.log(all);
+    //     for (let i = 0; i < all.length; i++) {
+    //         let singleAll = all[i];
+
+    //     }
 
 });
