5b6380e68143eaafe1ce9fc49311c478c2be0292
Angelika Tyburska <angelikatyburska@gmail.com>
2019-10-31 16:40:36 +0100

    Modified js file

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", function () {
             let total = 0;
             let total1 = (quantityValue1 * valueTable[0]);
             let total2 = (quantityValue2 * valueTable[1]);
-            let total3 = (quantityValue3 * valueTable[2]);
+            let total3 = val3.innerText;
             let total4 = quantityValue4;
             let total5 = quantityValue5;
 
@@ -49,19 +49,25 @@ document.addEventListener("DOMContentLoaded", function () {
                 const prices = [0, 25, 60];
                 if (quantityValue3 === "Basic") {
                     val3.innerText = "$" + prices[0];
+                    // total = total + total1 + total2 + total3;
+
                 } else if (quantityValue3 === "Professional") {
                     val3.innerText = "$" + prices[1];
+                    // total = total + total1 + total2 + total3;
+
                 } else if (quantityValue3 === "Premium") {
                     val3.innerText = "$" + prices[2];
+                    // total = total + total1 + total2 + total3;
+
                 }
-                total = total + total1 + total2 + total3;
-                button.innerText = total;
+                // total = total + total1 + total2 + total3;
+                // button.innerText = total;
 
 
             } else if (i === 3) {
                 val4.innerText = "$" + valueTable[3];
                 divs[3].style.visibility = "visible";
-                total = total + total1 + total2 + total3 + total4;
+                total = total + total1 + total2 + total4;
                 button.innerText = total;
 
 
@@ -69,7 +75,7 @@ document.addEventListener("DOMContentLoaded", function () {
             } else if (i === 4) {
                 val5.innerText = "$" + valueTable[4];
                 divs[4].style.visibility = "visible";
-                total = total + total1 + total2 + total3 + total4 + total5;
+                total = total + total1 + total2 + total4 + total5;
                 button.innerText = total;
             }
 
@@ -90,106 +96,5 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //     quantity.addEventListener("click", function () {
-    //         const value1 = 0.5;
-    //         const quantityValue = this.value;
-    //         val1.innerText = "$ " + quantityValue * value1;
-    //         qty1.innerText = `${quantityValue} *$${value1}`;
-    //         firstRow.style.visibility = "visible";
-    //     });
-    //     order.addEventListener("click", function (e) {
-    //         const value2 = 0.25;
-    //         const orderValue = this.value;
-    //         val2.innerText = "$ " + orderValue * value2;
-    //         qty2.innerText = `${orderValue} *$${value2}`;
-    //         secondRow.style.visibility = "visible";
-    //     });
-    //     package.addEventListener("click", function () {
-    //         const packageValue = this.value;
-    //         qty3.innerText = packageValue;
-    //         thirdRow.style.visibility = "visible";
-    //         const prices = [0, 25, 60];
-    //         if (packageValue === "Basic") {
-    //             val3.innerText = "$" + prices[0];
-    //         } else if (packageValue === "Professional") {
-    //             val3.innerText = "$" + prices[1];
-    //         } else if (packageValue === "Premium") {
-    //             val3.innerText = "$" + prices[2];
-    //         }
-    //     })
-    //     accounting.addEventListener("click", function () {
-    //         const value4 = 35;
-    //         val4.innerText = "$" + value4;
-    //         if (accounting.checked) {
-    //             fourthRow.style.visibility = "visible";
-    //         }
-    //     })
-    //     rental.addEventListener("click", function () {
-    //         const value5 = 5;
-    //         val5.innerText = "$" + value5;
-    //         if (rental.checked) {
-    //             fifthRow.style.visibility = "visible";
-    //         }
-    //     });
-    //     const all = document.querySelectorAll(".row-value");
-    //     console.log(all);
-    //     for (let i = 0; i < all.length; i++) {
-    //         let singleAll = all[i];
-
-    //     }
 
 });
